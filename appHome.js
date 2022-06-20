9a6c0f0e154ec295ffd5e15cebad61322dcfbe3a
Trong Tran <tran.dinh.trong@sun-asterisk.com>
2022-06-20 10:52:39 +0700

    test

--- appHome.js
+++ appHome.js
@@ -15,99 +15,108 @@ const apiUrl = 'https://slack.com/api';
  */
 
 const updateView = async(user) => {
-  
+  let blocks = [
+    {
+      "type": "section",
+      "text": {
+        "type": "plain_text",
+        "text": "This is the Home page of S*Task.",
+        "emoji": true
+      }
+    }
+  ]
   // Intro message - 
   
-  let blocks = [ 
-    {
-      type: "section",
-      text: {
-        type: "mrkdwn",
-        text: "*Welcome!* \nThis is a home for Stickers app. You can add small notes here!"
-      },
-      accessory: {
-        type: "button",
-        action_id: "add_note", 
-        text: {
-          type: "plain_text",
-          text: "Add a Stickie",
-          emoji: true
-        }
-      }
-    },
-    {
-      type: "context",
-      elements: [
-        {
-          type: "mrkdwn",
-          text: ":wave: Hey, my source code is on <https://glitch.com/edit/#!/apphome-demo-keep|glitch>!"
-        }
-      ]
-    },
-    {
-      type: "divider"
-    }
-  ];
+  // let blocks = [
+  //   {
+  //     type: "section",
+  //     text: {
+  //       type: "mrkdwn",
+  //       text: "*Welcome!* \nThis is a home for Stickers app. You can add small notes here!"
+  //     },
+  //     accessory: {
+  //       type: "button",
+  //       action_id: "add_note",
+  //       text: {
+  //         type: "plain_text",
+  //         text: "Add a Stickie",
+  //         emoji: true
+  //       }
+  //     }
+  //   },
+  //   {
+  //     type: "context",
+  //     elements: [
+  //       {
+  //         type: "mrkdwn",
+  //         text: ":wave: Hey, my source code is on <https://glitch.com/edit/#!/apphome-demo-keep|glitch>!"
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     type: "divider"
+  //   }
+  // ];
   
   
   // Append new data blocks after the intro - 
   
-  let newData = [];
-  
-  try {
-    const rawData = db.getData(`/${user}/data/`);
-    
-    newData = rawData.slice().reverse(); // Reverse to make the latest first
-    newData = newData.slice(0, 50); // Just display 20. BlockKit display has some limit.
-
-  } catch(error) {
-    //console.error(error); 
-  };
-  
-  if(newData) {
-    let noteBlocks = [];
-    
-    for (const o of newData) {
-      
-      const color = (o.color) ? o.color : 'yellow';
-      
-      let note = o.note;
-      if (note.length > 3000) {
-        note = note.substr(0, 2980) + '... _(truncated)_'
-        console.log(note.length);
-      }
-            
-      noteBlocks = [
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text: note
-          },
-          accessory: {
-            type: "image",
-            image_url: `https://cdn.glitch.com/0d5619da-dfb3-451b-9255-5560cd0da50b%2Fstickie_${color}.png`,
-            alt_text: "stickie note"
-          }
-        },
-        {
-          "type": "context",
-          "elements": [
-            {
-              "type": "mrkdwn",
-              "text": o.timestamp
-            }
-          ]
-        },
-        {
-          type: "divider"
-        }
-      ];
-      blocks = blocks.concat(noteBlocks);
-    
-    }
-    
-  }
+  // let newData = [];
+  //
+  // try {
+  //   const rawData = db.getData(`/${user}/data/`);
+  //
+  //   newData = rawData.slice().reverse(); // Reverse to make the latest first
+  //   newData = newData.slice(0, 50); // Just display 20. BlockKit display has some limit.
+  //
+  // } catch(error) {
+  //   //console.error(error);
+  // };
+  //
+  // if(newData) {
+  //   let noteBlocks = [];
+  //
+  //   for (const o of newData) {
+  //
+  //     const color = (o.color) ? o.color : 'yellow';
+  //
+  //     let note = o.note;
+  //     if (note.length > 3000) {
+  //       note = note.substr(0, 2980) + '... _(truncated)_'
+  //       console.log(note.length);
+  //     }
+  //
+  //     noteBlocks = [
+  //       {
+  //         type: "section",
+  //         text: {
+  //           type: "mrkdwn",
+  //           text: note
+  //         },
+  //         accessory: {
+  //           type: "image",
+  //           image_url: `https://cdn.glitch.com/0d5619da-dfb3-451b-9255-5560cd0da50b%2Fstickie_${color}.png`,
+  //           alt_text: "stickie note"
+  //         }
+  //       },
+  //       {
+  //         "type": "context",
+  //         "elements": [
+  //           {
+  //             "type": "mrkdwn",
+  //             "text": o.timestamp
+  //           }
+  //         ]
+  //       },
+  //       {
+  //         type: "divider"
+  //       }
+  //     ];
+  //     blocks = blocks.concat(noteBlocks);
+  //
+  //   }
+  //
+  // }
 
   // The final view -
   
